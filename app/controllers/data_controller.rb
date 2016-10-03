0daebc71e9587730ec0ca74d2218378d8eedb616
takenoko <tk.hrsk79@gmail.com>
2016-10-03 22:58:05 +0900

    save audio and view it in #index(chrome only)

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -69,6 +69,6 @@ class DataController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def datum_params
-      params.require(:datum).permit(:name)
+      params.require(:datum).permit(:name, :audio)
     end
 end
--- app/models/datum.rb
+++ app/models/datum.rb
@@ -1,8 +1,8 @@
 class Datum < ActiveRecord::Base
-    has_attached_file :audio
+    has_attached_file :audio, url: "/assets/audios/:id/:basename.:extension", path: "#{Rails.root}/public/assets/audios/:id/:basename.:extension"
     has_attached_file :video
     has_attached_file :gesture
 
     #どのMIME形式を許可するか、ないとエラーになる
-    validates_attachment_content_type :audio, content_type: { content_type: ["audio/mpeg", "audio/x-wav"] }
+    validates_attachment_content_type :audio, content_type: ["audio/mpeg", "audio/x-wav"]
 end
