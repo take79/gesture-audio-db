59fd756d21fa8b481498b6a94b79a42567bee23c
takenoko <tk.hrsk79@gmail.com>
2016-10-11 11:27:19 +0900

    realation and validation for gestures

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -5,9 +5,10 @@ class Datum < ActiveRecord::Base
         :medium => { :geometry => "640x480", :format => 'mp4' },
         :thumb => { :geometry => "100x100", :format => 'jpg', :time => 10 }
     }, :processors => [:transcoder]
-    has_attached_file :gesture
+    has_attached_file :gesture, url: "/assets/gestures/:id/:basename.:extention", path: "#{Rails.root}/public/assets/gestures/:id/:basename.:extension"
 
     #どのMIME形式を許可するか、ないとエラーになる
     validates_attachment_content_type :audio, content_type: ["audio/mpeg", "audio/x-wav"]
     validates_attachment_content_type :video, content_type: ["video/x-msvideo"]
+    validates_attachment_file_name :geture, matches: [/bvh\z/]
 end
