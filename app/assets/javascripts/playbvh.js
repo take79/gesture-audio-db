f23bc1ef06b6459d317f0596b346cf9655a49bc9
takenoko <tk.hrsk79@gmail.com>
2016-10-25 18:01:32 +0900

    各種同時再生

--- app/assets/javascripts/playbvh.js
+++ app/assets/javascripts/playbvh.js
@@ -1,12 +1,29 @@
 $(document).ready(function(){
   var v = document.getElementById("video");
   var a = document.getElementById("audio");
-  $(document).on('click', '#sync-play', function(){
+  $(document).on('click', '#av-sync-play', function(){
 //    $(this).attr('id', 'sync-pause');
     v.currentTime = 0;
     a.currentTime = 0;
     v.play();
     a.play();
+  });
+  $(document).on('click', '#ag-sync-play', function() {
+    a.currentTime = 0;
+    a.play();
+    $('#play_pause').trigger("click");
+  });
+  $(document).on('click', '#vg-sync-play', function(){
+    v.currentTime = 0;
+    v.play();
+    $('#play_pause').trigger("click");
+  });
+  $(document).on('click', '#all-sync-play', function(){
+    v.currentTime = 0;
+    a.currentTime = 0;
+    v.play();
+    a.play();
+    $('#play_pause').trigger("click");
   });
 //  $(document).on('click', '#sync-pause', function() {
 //    $(this).attr('id', 'sync-play');
