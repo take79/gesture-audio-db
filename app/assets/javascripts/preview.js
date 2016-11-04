282451436e8d0d05ba787ae978872c6dbc0c551b
takenoko <tk.hrsk79@gmail.com>
2016-11-04 15:41:25 +0900

    new preview準備

---
+++ app/assets/javascripts/preview.js
@@ -0,0 +1,16 @@
+$(window).load(function(){
+  console.log(document.getElementById("datum_video"));
+  document.getElementById("datum_video").addEventListener('change', function (event) {
+    var URL = URL || webkitURL;
+    var file = event.target.files[0];
+    console.log(file);
+    document.querySelector('video').src = URL.createObjectURL(file); // inputで選択した動画を再生する
+    $("#v-preview").show();
+  }, false);
+  document.getElementById("datum_audio").addEventListener('change', function (event) {
+    var URL = URL || webkitURL;
+    var file = event.target.files[0];
+    document.querySelector('audio').src = URL.createObjectURL(file);
+    $("#a-preview").css('display', 'inline');
+  }, false);
+})
