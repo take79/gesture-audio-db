8caa75c22e09f06b14378a0599f50cc7253405ea
takenoko <tk.hrsk79@gmail.com>
2016-11-07 14:35:14 +0900

    アップした時にrenderer表示、src変更

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -3,7 +3,6 @@ $(window).load(function(){
   document.getElementById("datum_video").addEventListener('change', function (event) {
     var URL = URL || webkitURL;
     var file = event.target.files[0];
-    console.log(file);
     document.querySelector('video').src = URL.createObjectURL(file); // inputで選択した動画を再生する
     $("#v-preview").show();
   }, false);
@@ -12,5 +11,11 @@ $(window).load(function(){
     var file = event.target.files[0];
     document.querySelector('audio').src = URL.createObjectURL(file);
     $("#a-preview").css('display', 'inline');
+  }, false);
+  document.getElementById("datum_gesture").addEventListener('change', function (event) {
+    var URL = URL || webkitURL;
+    var file = event.target.files[0];
+    $('#renderer').attr('data-src', URL.createObjectURL(file));
+    $("#g-preview").show();
   }, false);
 })
