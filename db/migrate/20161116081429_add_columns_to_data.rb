e3616d262520d5934f3593a8e743ba63b80683a7
takenoko <tk.hrsk79@gmail.com>
2016-11-16 17:16:57 +0900

    migrate id columns

---
+++ db/migrate/20161116081429_add_columns_to_data.rb
@@ -0,0 +1,7 @@
+class AddColumnsToData < ActiveRecord::Migration
+  def change
+    add_column :data, :actor_id, :integer
+    add_column :data, :topic_id, :integer
+    add_column :data, :gesture_tag_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114061303) do
+ActiveRecord::Schema.define(version: 20161116081429) do
 
   create_table "actors", force: :cascade do |t|
     t.string   "gender",     limit: 255
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 20161114061303) do
     t.string   "gesture_content_type", limit: 255
     t.integer  "gesture_file_size",    limit: 4
     t.datetime "gesture_updated_at"
+    t.integer  "actor_id",             limit: 4
+    t.integer  "topic_id",             limit: 4
+    t.integer  "gesture_tag_id",       limit: 4
   end
 
   create_table "gesture_tags", force: :cascade do |t|
