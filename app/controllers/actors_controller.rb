54feb8120a40ce3353ee7e998627b70abf1f2e76
takenoko <tk.hrsk79@gmail.com>
2016-11-18 00:02:55 +0900

    add columns again, add links

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -69,6 +69,6 @@ class ActorsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def actor_params
-      params.require(:actor).permit(:gender, :age)
+      params.require(:actor).permit(:name, :gender, :age)
     end
 end
