16f9478d0dd5de3c149e3cc6be5d80f5757fc14c
takenoko <tk.hrsk79@gmail.com>
2016-12-08 18:05:12 +0900

    devise初期設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :gesture_tags
   resources :topics
   resources :actors
   resources :data
+  root 'data#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
