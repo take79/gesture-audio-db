a3881a9fd629bd69225d4d295cbd7e9e65dbbd82
takenoko <tk.hrsk79@gmail.com>
2016-12-09 13:14:48 +0900

    remove unused defs

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
-  def new
-    super
-  end
-
-  def create
-    super
-  end
 end
