eb8636f14bb7b642b4ce1af64c55a76276c30d28
take79 <tk.hrsk79@gmail.com>
2017-05-13 03:52:04 +0900

    とりあえずwavとかmp3以外は弾くように

--- app/assets/javascripts/generate_result.js
+++ app/assets/javascripts/generate_result.js
@@ -3,10 +3,13 @@ $(document).ready( function(){
   document.getElementById('input_audio').addEventListener('change', function (event) {
     var URL = URL || webkitURL;
     var file = event.target.files[0];
-    document.querySelector('audio').src = URL.createObjectURL(file);
-    $("#a-result").css('display', 'inline');
-    $('#g-result').show();
-    $('#res-sync-play').show();
-    bvhlabs.init($('#renderer'))
+    console.log(file.type);
+    if(file.type == 'audio/wav' || file.type == 'audio/mp3'){
+      document.querySelector('audio').src = URL.createObjectURL(file);
+      $("#a-result").css('display', 'inline');
+      $('#g-result').show();
+      $('#res-sync-play').show();
+      bvhlabs.init($('#renderer'))
+    }
   }, false);
 })
